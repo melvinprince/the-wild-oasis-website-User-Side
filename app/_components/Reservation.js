3c567b69d97b53887fbf0c36d12424b8eef3d8a8
Melvin <melvinmps11301@gmail.com>
2025-01-26 13:42:15 +0300

    implemented feature to only enable logged in user to reserve

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,8 +1,12 @@
 import DateSelector from "@/app/_components/DateSelector";
 import ReservationForm from "@/app/_components/ReservationForm";
 import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
+import { auth } from "@/app/_lib/auth";
+import LoginMessage from "@/app/_components/LoginMessage";
 
 async function Reservation({ cabin }) {
+  const session = await auth();
+
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
@@ -15,7 +19,11 @@ async function Reservation({ cabin }) {
           bookedDates={bookedDates}
           cabin={cabin}
         />
-        <ReservationForm cabin={cabin} />
+        {session?.user ? (
+          <ReservationForm cabin={cabin} user={session.user} />
+        ) : (
+          <LoginMessage />
+        )}
       </div>
     </div>
   );
